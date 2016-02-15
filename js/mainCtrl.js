a2ae827b07fbce14b275a9b9cd293abffbd7c701
Katie Slater <katiemyfriend@hotmail.com>
2016-02-15 09:04:03 -0700

    working on step4

---
+++ js/mainCtrl.js
@@ -0,0 +1,19 @@
+var app = angular.module('quoteBook');
+
+app.controller('mainCtrl', function($scope, dataService){
+
+
+  $scope.getQuotes = function() {
+    $scope.quotes = dataService.getData();
+  }
+  $scope.getQuotes();
+
+  $scope.addQuotes = function() {
+    dataService.addData($scope.quote);
+  }
+  $scope.removeQuotes = function() {
+    dataService.removeData($scope.quote);
+  }
+
+
+});
